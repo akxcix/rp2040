e140be1de7619a3ba39a3b869213c185c87412b7
akxcix <git@akxcix.com>
2024-06-05 15:28:33 -0400

    add minimal code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use rp2040_hal as hal;
+use panic_halt as _;
 
-mod boot {}
+use embedded_hal::digital::OutputPin;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 
-#[panic_handler]
-fn panic(_info:&PanicInfo) -> ! {
-    loop {}
+#[rp2040_hal::entry]
+fn entry() -> ! {
+
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+    
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0, 
+        pac.PADS_BANK0, 
+        sio.gpio_bank0, 
+        &mut pac.RESETS,
+    );
+
+    let mut led = pins.gpio13.into_push_pull_output();
+    loop{
+        led.set_high().unwrap();
+    }
 }
